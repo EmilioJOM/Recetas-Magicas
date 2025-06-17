8e2b2cfffa9dd691221e3e762139439039081433
EmilioJOM <emijesus21@gmail.com>
2025-06-17 16:28:35 -0300

    Update RecipeDetailDto.java

--- API/src/main/java/com/cocinaapp/RecetasMagicas/recipe/dto/RecipeDetailDto.java
+++ API/src/main/java/com/cocinaapp/RecetasMagicas/recipe/dto/RecipeDetailDto.java
@@ -10,7 +10,6 @@ public class RecipeDetailDto {
     private String title;
     private String description;
     private Integer servings;
-    private Long tipoId;
     private String mainPhoto;
     private String authorAlias;
     private LocalDateTime createdAt;
