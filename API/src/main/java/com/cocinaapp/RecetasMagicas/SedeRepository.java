f7bfcd61bd5d00cb354069e2345ce7a02cd36436
EmilioJOM <emijesus21@gmail.com>
2025-06-25 01:39:35 -0300

    borrar SedeRepository
    aparentemente lo cree por accidente

--- API/src/main/java/com/cocinaapp/RecetasMagicas/SedeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cocinaapp.RecetasMagicas;
-
-import com.cocinaapp.RecetasMagicas.course.model.Sede;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SedeRepository extends JpaRepository<Sede, Long> {}
